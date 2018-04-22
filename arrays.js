f13114c0d3e0d873bf018e07b3db1e9a53c3d10f
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-22 21:00:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
   ]
   
-function addElementToBeginningOfArray([array], "element") {
+function addElementToBeginningOfArray([array], 'element') {
   ["element", ...array];
   return array;
 }
