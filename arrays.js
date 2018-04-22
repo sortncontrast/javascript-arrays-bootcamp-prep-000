5a9382391a6d20e491b259428f7b91ba10dfbed4
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-22 21:35:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
   'skittles'
   ]
 
-//Test 2 and 3: Without Changing The Original Array
+//Test 2 and 3: Adding to Beginning Without Changing the Original Array
 function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array];
   return newArray;
@@ -14,7 +14,7 @@ function addElementToBeginningOfArray(array, element) {
 
 addElementToBeginningOfArray([1], 'foo');
 
-//Test 4 and 5: With Changing The Original Array
+//Test 4 and 5: Adding to Beginning With Changing the Original Array
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
@@ -22,7 +22,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 destructivelyAddElementToBeginningOfArray([1], 'foo');
 
-//Test 6 and 7: Without Changing The Original Array
+//Test 6 and 7: Adding to End Without Changing the Original Array
 function addElementToEndOfArray(array, element) {
   var newArray = [...array, element];
   return newArray;
@@ -30,7 +30,7 @@ function addElementToEndOfArray(array, element) {
 
 addElementToEndOfArray([1], 'foo');
 
-//Test 8 and 9: With Changing The Original Array
+//Test 8 and 9: Adding to End With Changing the Original Array
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
@@ -41,4 +41,10 @@ destructivelyAddElementToEndOfArray([1], 'foo');
 //Test 10: Return Element At Specific Array Index
 function accessElementInArray(array, index) {
   return array[index];
+}
+
+//Test 11 and 12: Remove from Beginning With Changing the Original Array
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 }
